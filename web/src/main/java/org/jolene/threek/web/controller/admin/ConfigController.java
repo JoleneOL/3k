f465e219d7ae0d9f2ebe26fa1e76c3a6d29c1cad
allan <lqc_huli@163.com>
2016-01-08 23:34:20 +0800

    add configPage Test add jquery.util.js complete config page

--- web/src/main/java/org/jolene/threek/web/controller/admin/ConfigController.java
+++ web/src/main/java/org/jolene/threek/web/controller/admin/ConfigController.java
@@ -4,13 +4,19 @@ import org.jolene.threek.SystemConfig;
 import org.jolene.threek.common.exception.ForbiddenException;
 import org.jolene.threek.entity.Login;
 import org.jolene.threek.service.AppService;
+import org.jolene.threek.support.InterestReward;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @author Jolene
@@ -27,7 +33,11 @@ public class ConfigController {
      * @return 页面
      */
     @RequestMapping(method = RequestMethod.GET, value = "/config")
-    public String index(@AuthenticationPrincipal Login login) {
+    public String index(@AuthenticationPrincipal Login login, Model model, HttpServletRequest request) {
+        model.addAttribute("saveResult", request.getSession().getAttribute("saveResult"));
+        model.addAttribute("saveMsg", request.getSession().getAttribute("saveMsg"));
+        request.getSession().setAttribute("saveResult", null);
+        request.getSession().setAttribute("saveMsg", null);
         // 如果已配置 需要获得ROOT才可以配置
         if (appService.currentSystemConfig().isConfigRequired())
             return "admin/config";
@@ -41,15 +51,50 @@ public class ConfigController {
 
     @RequestMapping(method = RequestMethod.POST, value = "/config")
     @Transactional
-    public String change(@AuthenticationPrincipal Login login, String title) {
+    public String change(@AuthenticationPrincipal Login login,
+                         SystemConfig requestConfig,
+                         String tags,
+                         String rewardSerialization,
+                         HttpServletRequest request
+    ) {
         // 不进行校验 直接投入到系统中去
         SystemConfig systemConfig = appService.currentSystemConfig();
         systemConfig.setConfigRequired(false);
 
-        systemConfig.setTitle(title);
+        systemConfig.setTitle(requestConfig.getTitle());
+        systemConfig.setUrl(requestConfig.getUrl());
+        systemConfig.setWelcomeMessage(requestConfig.getWelcomeMessage());
+        systemConfig.setWelcomeFeatures(tags.split(","));
+        systemConfig.setIndexTopNotice(requestConfig.getIndexTopNotice());
+        systemConfig.setIndexBottomNotice(requestConfig.getIndexBottomNotice());
+        systemConfig.setStock(requestConfig.getStock());
+        systemConfig.setMaxOrders(requestConfig.getMaxOrders());
+        systemConfig.setMaxLots(requestConfig.getMaxLots());
+        systemConfig.setQueueDays(requestConfig.getQueueDays());
+        systemConfig.setRate(requestConfig.getRate());
+        systemConfig.setMaxOperateHours(requestConfig.getMaxOperateHours());
+        systemConfig.setInspectStartDayOfMonth(requestConfig.getInspectStartDayOfMonth());
+        systemConfig.setInspectEndDayOfMonth(requestConfig.getInspectEndDayOfMonth());
+        String[] interestRewardsInfo = rewardSerialization.split("\\|");
+        Map<Integer, InterestReward> interestRewardMap = new HashMap<>();
+        for (int i = 1; i <= interestRewardsInfo.length; i++) {
+            String[] rewordDetail = interestRewardsInfo[i].split(",");
+            InterestReward interestReward = new InterestReward();
+            interestReward.setRate(Double.parseDouble(rewordDetail[0]));
+            interestReward.setReach(Integer.parseInt(rewordDetail[1]));
+            interestReward.setMax(Integer.parseInt(rewordDetail[2]));
+            interestRewardMap.put(i, interestReward);
+        }
+        systemConfig.setLeaderStopRecommends(requestConfig.getLeaderStopRecommends());
+        systemConfig.setLeaderStopMembers(requestConfig.getLeaderStopMembers());
+        systemConfig.setLeaderStopInvests(requestConfig.getLeaderStopInvests());
+        systemConfig.setLeaderRate(requestConfig.getLeaderRate());
 
         //保存到系统中
         appService.saveCurrentSystemConfig();
+
+        request.getSession().setAttribute("saveResult", 1);
+        request.getSession().setAttribute("saveMsg", "保存成功");
 
         if (login == null)
             return "redirect:/";
--- web/src/test/java/org/jolene/threek/web/controller/admin/pages/ConfigPage.java
+++ web/src/test/java/org/jolene/threek/web/controller/admin/pages/ConfigPage.java
@@ -1,11 +1,17 @@
 package org.jolene.threek.web.controller.admin.pages;
 
 import org.jolene.threek.SystemConfig;
+import org.jolene.threek.support.InterestReward;
 import org.jolene.threek.web.pages.AbstractPage;
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+import java.util.Map;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -16,7 +22,54 @@ public class ConfigPage extends AbstractPage {
 
     @FindBy(css = "form")
     private WebElement form;
+    @FindBy(css = "input[name='title']")
     private WebElement title;
+    @FindBy(css = "input[name='url']")
+    private WebElement url;
+    @FindBy(css = "textarea[name='welcomeMessage']")
+    private WebElement welcomeMessage;
+    @FindBy(css = "input[name='tags']")
+    private WebElement tags;
+    @FindBy(css = "#tags_tagsinput")
+    private WebElement tagsInputDiv;
+    @FindBy(css = "#tags_tag")
+    private WebElement tagsInput;
+    @FindBy(css = "textarea[name='indexTopNotice']")
+    private WebElement indexTopNotice;
+    @FindBy(css = "textarea[name='indexBottomNotice']")
+    private WebElement indexBottomNotice;
+    @FindBy(css = "input[name='stock']")
+    private WebElement stock;
+    @FindBy(css = "input[name='maxOrders']")
+    private WebElement maxOrders;
+    @FindBy(css = "input[name='maxLots']")
+    private WebElement maxLots;
+    @FindBy(css = "input[name='queueDays']")
+    private WebElement queueDays;
+    @FindBy(css = "input[name='rate']")
+    private WebElement rate;
+    @FindBy(css = "input[name='maxOperateHours']")
+    private WebElement maxOperateHours;
+    @FindBy(css = "input[name='inspectStartDayOfMonth']")
+    private WebElement inspectStartDayOfMonth;
+    @FindBy(css = "input[name='inspectEndDayOfMonth']")
+    private WebElement inspectEndDayOfMonth;
+    @FindBy(css = "input[name='directRewardRate']")
+    private WebElement directRewardRate;
+    @FindBy(css = "input[name='directRewardRate2']")
+    private WebElement directRewardRate2;
+    @FindBy(css = "input[name='interestRewards']")
+    private WebElement interestRewards;
+    @FindBy(css = "input[name='leaderStopRecommends']")
+    private WebElement leaderStopRecommends;
+    @FindBy(css = "input[name='leaderStopMembers']")
+    private WebElement leaderStopMembers;
+    @FindBy(css = "input[name='leaderStopInvests']")
+    private WebElement leaderStopInvests;
+    @FindBy(css = "input[name='leaderRate']")
+    private WebElement leaderRate;
+
+    //    private WebElement
     @FindBy(css = "button[class~=btn-primary]")
     private WebElement submitButton;
 
@@ -33,10 +86,91 @@ public class ConfigPage extends AbstractPage {
     public void submit(SystemConfig systemConfig) {
         title.clear();
         title.sendKeys(systemConfig.getTitle());
+        url.clear();
+        url.sendKeys(systemConfig.getUrl());
+        welcomeMessage.clear();
+        welcomeMessage.sendKeys(systemConfig.getWelcomeMessage());
 
-        // TODO 更多的输入操作
+        String[] welcomeFeatures = systemConfig.getWelcomeFeatures();
+        String welcomeFeatureStr = "";
+        //执行点击事件删除原有的tag
+        tagsInputDiv.findElements(By.className("tag")).forEach(t -> {
+            t.findElement(By.cssSelector("a")).click();
+        });
+        tags.clear();
+        for (String welcomeFeature : welcomeFeatures) {
+            tagsInput.clear();
+            tagsInput.sendKeys(welcomeFeature);
+            welcomeFeatureStr += welcomeFeature + ",";
+            indexTopNotice.click();
+        }
+
+        List<WebElement> tagsSpan = tagsInputDiv.findElements(By.className("tag"));
+        assertThat(welcomeFeatures.length)
+                .isEqualTo(tagsSpan.size());
+        assertThat(welcomeFeatures[0])
+                .isEqualTo(tagsSpan.get(0).findElement(By.tagName("span")).getText().trim());
+        assertThat(tags.getAttribute("value").trim())
+                .isEqualTo(welcomeFeatureStr.toString().substring(0, welcomeFeatureStr.length() - 1));
+
+        indexTopNotice.clear();
+        indexTopNotice.sendKeys(systemConfig.getIndexTopNotice());
+        indexBottomNotice.clear();
+        indexBottomNotice.sendKeys(systemConfig.getIndexBottomNotice());
+        stock.clear();
+        stock.sendKeys(String.valueOf(systemConfig.getStock()));
+        maxOrders.clear();
+        maxOrders.sendKeys(String.valueOf(systemConfig.getMaxOrders()));
+        maxLots.clear();
+        maxLots.sendKeys(String.valueOf(systemConfig.getMaxLots()));
+        queueDays.clear();
+        queueDays.sendKeys(String.valueOf(systemConfig.getQueueDays()));
+        rate.clear();
+        rate.sendKeys(String.valueOf(systemConfig.getRate()));
+        maxOperateHours.clear();
+        maxOperateHours.sendKeys(String.valueOf(systemConfig.getMaxOperateHours()));
+        inspectStartDayOfMonth.clear();
+        inspectStartDayOfMonth.sendKeys(String.valueOf(systemConfig.getInspectStartDayOfMonth()));
+        inspectEndDayOfMonth.clear();
+        inspectEndDayOfMonth.sendKeys(String.valueOf(systemConfig.getInspectEndDayOfMonth()));
+
+        Map<Integer, InterestReward> interestRewardMap = systemConfig.getInterestRewards();
+
+        interestRewardMap.forEach((sg, interestReward) -> {
+            WebElement interestRate = form.findElement(By.cssSelector("input[name='interestRate" + sg + "']"));
+            interestRate.clear();
+            interestRate.sendKeys(String.valueOf(interestReward.getRate()));
+            WebElement interestReach = form.findElement(By.cssSelector("input[name='interestReach'" + sg + "]"));
+            interestReach.clear();
+            interestReach.sendKeys(String.valueOf(interestReward.getReach()));
+            WebElement interestMax = form.findElement(By.cssSelector("input[name='interestMax" + sg + "']"));
+            interestMax.clear();
+            interestMax.sendKeys(String.valueOf(interestReward.getMax()));
+        });
+
+        leaderStopRecommends.clear();
+        leaderStopRecommends.sendKeys(String.valueOf(systemConfig.getLeaderStopRecommends()));
+        leaderStopMembers.clear();
+        leaderStopMembers.sendKeys(String.valueOf(systemConfig.getLeaderStopMembers()));
+        leaderStopInvests.clear();
+        leaderStopInvests.sendKeys(String.valueOf(systemConfig.getLeaderStopInvests()));
+        leaderRate.clear();
+        leaderRate.sendKeys(String.valueOf(systemConfig.getLeaderRate()));
 
         submitButton.click();
+
+        PageFactory.initElements(webDriver, this);
+        WebElement msg = webDriver.findElement(By.cssSelector("#gritter-notice-wrapper p"));
+        assertThat(msg.getText())
+                .isEqualTo("保存成功");
+        webDriver.navigate().refresh();
+        PageFactory.initElements(webDriver, this);
+        try {
+            msg = webDriver.findElement(By.cssSelector("#gritter-notice-wrapper"));
+            assertThat(true).isTrue();
+        } catch (NoSuchElementException ignored) {
+        }
+
 //        return PageFactory.initElements(webDriver, ConfigPage.class);
     }
 }
